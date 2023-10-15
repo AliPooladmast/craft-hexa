2e9971fa842f3f0d3bd9a20a3ee114e9fcf36315
ali.pooladmast <ali.pooladmast.gh@gmail.com>
2023-10-15 20:37:07 +0200

    deploy

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  backgroundColor = environment;
+}
